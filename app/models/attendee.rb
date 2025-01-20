25d436f12cbb15def6db811809f5102f8a4f0db4
Danielle Cardona <danielle.cardona23@gmail.com>
2025-01-20 15:04:08 -0700

    add model validations

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,4 +2,8 @@ class Attendee < ApplicationRecord
   has_many :schedule_attendees
   has_many :schedules, through: :schedule_attendees
   has_many :shows, through: :schedules
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
